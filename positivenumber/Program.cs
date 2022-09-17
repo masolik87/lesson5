05ca45c778c1941fa274280d4d6e9204b5d05138
Дмитрий <dimon20.07@mail.ru>
2022-09-17 11:59:17 +0300

    positivenumber

---
+++ positivenumber/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int size = 5;
+
+int[] array = new int[size];
+int resultPositive = 0;
+for (int i = 0; i < array.Length; i++) // array.Length = size
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + "\t");
+    if (array[i] % 2 == 0) resultPositive ++;
+}
+Console.WriteLine("\n" + $"Positive: {resultPositive}");
